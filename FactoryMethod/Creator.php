1e028befdc5891d3093a7dd35e7286855563f0e3
承建 <kevin_chengjian@163.com>
2021-04-12 23:12:55 +0800

    Factory Method

---
+++ FactoryMethod/Creator.php
@@ -0,0 +1,111 @@
+<?php
+
+/**
+ * Class Creator
+ */
+abstract class Creator
+{
+    /**
+     * @return Product
+     */
+    abstract public function factoryMethod(): Product;
+
+    /**
+     * @return mixed
+     */
+    public function someOperation()
+    {
+        return $this->factoryMethod()->operation();
+    }
+}
+
+/**
+ * Class ConcreteCreatorA
+ */
+class ConcreteCreatorA extends Creator
+{
+    /**
+     * @return Product
+     */
+    public function factoryMethod(): Product
+    {
+        return new ConcreteProductA();
+    }
+}
+
+/**
+ * Class ConcreteCreatorB
+ */
+class ConcreteCreatorB extends Creator
+{
+    /**
+     * @return Product
+     */
+    public function factoryMethod(): Product
+    {
+        return new ConcreteProductB();
+    }
+}
+
+/**
+ * Interface Product
+ */
+interface Product
+{
+    public function operation();
+}
+
+/**
+ * Class ConcreteProductA
+ */
+class ConcreteProductA implements Product
+{
+    /**
+     * @return mixed
+     */
+    public function operation()
+    {
+        echo "ConcreteProductA\n";
+    }
+}
+
+
+/**
+ * Class ConcreteProductB
+ */
+class ConcreteProductB implements Product
+{
+    /**
+     * @return mixed
+     */
+    public function operation()
+    {
+        echo "ConcreteProductB\n";
+    }
+}
+
+
+// 测试
+function test(Creator $creator)
+{
+    return $creator->someOperation();
+}
+
+
+test(new ConcreteCreatorA());
+test(new ConcreteCreatorB());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
